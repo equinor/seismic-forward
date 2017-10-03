9c7b2cc8bad262a70e4b35a0b8a9a864d41a58bb
Pål Dahle <pal.dahle@nr.no>
2017-10-03 17:54:50 +0200

    Moved compare_traces.cpp to geo2seis directory and updated make_compare_traces.sh

--- make_compare_traces.sh
+++ make_compare_traces.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR=../Geo2Seis
 CUR_DIR=`basename "$PWD"`
-EXE_DIR=compare_segy_traces
+OBJ_DIR=CMakeFiles/geo2seis_lib.dir
 EXE=compare_traces
 
 if [ ${CUR_DIR} = "Geo2Seis" ]; then
@@ -12,40 +12,38 @@ if [ ${CUR_DIR} = "Geo2Seis" ]; then
     exit;
 fi
 
-if [ -e ${EXE_DIR} ]; then
-    echo "Directory compare_segy_traces exists"
-else
-    echo "Making directory compare_segy_traces"
-    mkdir ${EXE_DIR}
+if  [ -e ${OBJ_DIR}/geo2seis/compare_traces.o ] ; then
+  echo "Removing old object file compare_traces.o"
+  rm ${OBJ_DIR}/geo2seis/compare_traces.o
+fi
+if [ -e ${EXE} ] ; then
+  echo "Removing old executable"
+  rm ${EXE}
 fi
 
-echo "Removing old executable and object file"
-rm ${EXE_DIR}/compare_traces.o
-rm ${EXE}
-
-echo "Compiling compare_segy_traces/compare.cpp"
+echo "Compiling compare.cpp"
 g++ -c -O2 \
     -I${ROOT_DIR}  \
-    ${ROOT_DIR}/${EXE_DIR}/compare_traces.cpp -o compare_segy_traces/compare_traces.o
+    ${ROOT_DIR}/geo2seis/compare_traces.cpp -o ${OBJ_DIR}/geo2seis/compare_traces.o
 
 echo "Linking compare.exe"
 g++ -I${ROOT_DIR} \
     -I${ROOT_DIR}/nr \
     -I${ROOT_DIR}/geo2seis \
     -I/opt/software/intel/compilers_and_libraries_2016.2.181/linux/tbb/include \
-    ${EXE_DIR}/compare_traces.o                                             \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/iotools/fileio.cpp.o               \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/segy/segy.cpp.o                    \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/segy/segytrace.cpp.o               \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/segy/segygeometry.cpp.o            \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/segy/traceheader.cpp.o             \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/segy/commonheaders.cpp.o           \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/math/constants.cpp.o               \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/iotools/stringtools.cpp.o          \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/iotools/logkit.cpp.o               \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/volume/volume.cpp.o                \
-    CMakeFiles/geo2seis_lib.dir/nr/nrlib/surface/surfaceio.cpp.o            \
-    CMakeFiles/geo2seis_lib.dir/3rd-party/boost/system/error_code.cpp.o     \
-    CMakeFiles/geo2seis_lib.dir/3rd-party/boost/filesystem/operations.cpp.o \
-    CMakeFiles/geo2seis_lib.dir/3rd-party/boost/filesystem/path.cpp.o       \
+    ${OBJ_DIR}/geo2seis/compare_traces.o                   \
+    ${OBJ_DIR}/nr/nrlib/iotools/fileio.cpp.o               \
+    ${OBJ_DIR}/nr/nrlib/segy/segy.cpp.o                    \
+    ${OBJ_DIR}/nr/nrlib/segy/segytrace.cpp.o               \
+    ${OBJ_DIR}/nr/nrlib/segy/segygeometry.cpp.o            \
+    ${OBJ_DIR}/nr/nrlib/segy/traceheader.cpp.o             \
+    ${OBJ_DIR}/nr/nrlib/segy/commonheaders.cpp.o           \
+    ${OBJ_DIR}/nr/nrlib/math/constants.cpp.o               \
+    ${OBJ_DIR}/nr/nrlib/iotools/stringtools.cpp.o          \
+    ${OBJ_DIR}/nr/nrlib/iotools/logkit.cpp.o               \
+    ${OBJ_DIR}/nr/nrlib/volume/volume.cpp.o                \
+    ${OBJ_DIR}/nr/nrlib/surface/surfaceio.cpp.o            \
+    ${OBJ_DIR}/3rd-party/boost/system/error_code.cpp.o     \
+    ${OBJ_DIR}/3rd-party/boost/filesystem/operations.cpp.o \
+    ${OBJ_DIR}/3rd-party/boost/filesystem/path.cpp.o       \
     -o ${EXE}
