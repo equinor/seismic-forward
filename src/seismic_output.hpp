feb2a4a0028cfa646eb67d3cc23a5a1fc5d9f31d
Pål Dahle <pal.dahle@nr.no>
2023-09-20 13:02:38 +0200

    GEOS-42 White noise added to same time interval for all NMO offsets (same as for non-NMO which equals 0 offset)

--- src/seismic_output.hpp
+++ src/seismic_output.hpp
@@ -41,14 +41,14 @@ public:
                    bool                        time,
                    bool                        nmo);
 
-  void WriteSegyGather(NRLib::Grid2D<double>     & data_gather,
-                       NRLib::SegY               & segyout,
-                       const std::vector<double>   twt_0,
-                       const std::vector<double>   offset_vec,
-                       bool                        time,
-                       double                      x,
-                       double                      y,
-                       bool                        nmo);
+  void WriteSegyGather(const NRLib::Grid2D<double> & data_gather,
+                       NRLib::SegY                 & segyout,
+                       const std::vector<double>   & twt_0,
+                       const std::vector<double>   & offset_vec,
+                       bool                          time,
+                       double                        x,
+                       double                        y,
+                       bool                          nmo);
 
   void WriteZeroSegyGather(NRLib::SegY               & segyout,
                            const std::vector<double>   offset_vec,
