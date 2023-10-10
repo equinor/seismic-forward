e547e50d558724f5a79ed45719df2e0ed53c38db
Pål Dahle <pal.dahle@nr.no>
2023-10-10 15:08:09 +0200

    GEOS-42 Made sure last sample is included in SegY files for low offsets when writing seismic data to file

--- src/seismic_output.hpp
+++ src/seismic_output.hpp
@@ -50,6 +50,15 @@ public:
                        double                        y,
                        bool                          nmo);
 
+  void WriteSegyGather2(const NRLib::Grid2D<double> & data_gather,
+                       NRLib::SegY                 & segyout,
+                       const std::vector<double>   & twt_0,
+                       const std::vector<double>   & offset_vec,
+                       bool                          time,
+                       double                        x,
+                       double                        y,
+                       bool                          nmo);
+
   void WriteZeroSegyGather(NRLib::SegY               & segyout,
                            const std::vector<double>   offset_vec,
                            double                      x,
